490c8efa4791d134c5cacd495b920951bbacd05c
Lycheejam <lycheejamwow@gmail.com>
2019-04-13 23:48:34 +0900

    fix message

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const Client = require('@line/bot-sdk').Client;
 
 exports.handler = (event, context, callback) => {
-  const client = new Client({
+  let client = new Client({
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
     channelSecret: process.env.CHANNEL_SECRET
   });
   
-  var message = event.detail['project-name'];
+  let message = '';
   
   if (event.detail['build-status'] === 'SUCCEEDED') {
-    message = message + '\r\n' + 'ビルド成功';
+    message = event.time + '\n'
+            + 'プロジェクト名：' + event.detail['project-name'] + '\n'
+            + 'ステータス：ビルド成功' + '\n';
   } else if (event.detail['build-status'] === 'FAILED') {
-    message = message + '\r\n' + 'ビルド失敗';
+    message = event.time + '\n'
+            + 'プロジェクト名：' + event.detail['project-name'] + '\n'
+            + 'ステータス：ビルド失敗' + '\n';
   } else if (event.detail['build-status'] === 'STOPPED') {
-    message = message + '\r\n' + 'ビルド停止';
+    message = event.time + '\n'
+            + 'プロジェクト名：' + event.detail['project-name'] + '\n'
+            + 'ステータス：ビルド停止' + '\n';
   }
   
   client.pushMessage(process.env.USERID, { type: 'text', text: message });
